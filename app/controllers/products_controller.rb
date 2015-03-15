ea6d4c5f18631179ea1e6928d49feb954857ff52
wagurano <wagurano@gmail.com>
2015-03-16 00:47:41 +0900

    Fix acceptance validation
    strong parameter and checkbox tag

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :value)
+      params.require(:product).permit(:title, :description, :value, :are_you_sure)
     end
 end
