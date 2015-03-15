d75284d1284243d2a904b57e5104860dd529e6e4
wagurano <wagurano@gmail.com>
2015-03-15 23:53:02 +0900

    Product validates title, description, value presence true, valdiates value numericality, are_you_sure acceptance true

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, :description, :value, presence: true
+  validates :value, numericality: { greater_than_or_equal_to: 100, less_than_or_equal_to: 1000 }
+  validates :are_you_sure, acceptance: true
 end
