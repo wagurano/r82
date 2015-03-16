c03e3bb2593edf507d8c524c65a7078ccd572f55
wagurano <wagurano@gmail.com>
2015-03-17 01:07:06 +0900

    scaffold person

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :people
+
   resources :products
 
   root 'products#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315141744) do
+ActiveRecord::Schema.define(version: 20150316160428) do
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
