d22deaecc41847a1638738dd270fc388f5842cfe
wagurano <wagurano@gmail.com>
2015-03-30 00:30:40 +0900

    add email format validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,5 @@ class Person < ActiveRecord::Base
   # better
   # validates :email_confirmation, presence: true
   validates :name, exclusion: { in: %w(admin root) }
+  validates :email, format: { with: /\A[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[.]{1}[A-Za-z]{2,5}\z/, message: 'malformed email address' }
 end
