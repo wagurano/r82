530ccf2b07fcffe2e46eef8c1aa5aba39696fe8e
wagurano <wagurano@gmail.com>
2015-03-31 02:25:40 +0900

    updated person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,8 +8,13 @@ class Person < ActiveRecord::Base
   # validates :language, inclusion: { in: I18n.available_locales.map(&:to_s) }
   validates :language, inclusion: { in: %w(ko en jp cn) }
   # length
-  # validates :name, length: { minimum :2 }
-  # validates :name, length: { maximum :10 }
-  # validates :name, length: { in: 3..7 }
+  # validates :name, length: { minimum: 2, too_short: "at least %{count} words" }
+  # validates :name, length: { maximum: 10, too_long: "at most %{count} words"}
+  # validates :name, length: { in: 3..7, wrong_length: "out of range" } # not working
   # validates :name, length: { is: 5 }
+  validates :name, length: { minimum: 2, tokenizer: lambda { |s| s.scan(/\w+/) } }
+
+  # raise exception
+  validates :name, presence: { strict: true }
+  # validates :name, presence: { strict: Exception }
 end
